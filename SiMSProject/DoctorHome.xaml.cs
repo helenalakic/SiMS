fbaa9a7eea360c7703f95d88fe673776fd0b325a
helenalakic <hlakic21@gmail.com>
2022-08-06 09:28:05 +0200

    Implemented search

--- SiMSProject/DoctorHome.xaml.cs
+++ SiMSProject/DoctorHome.xaml.cs
@@ -82,7 +82,39 @@ namespace SiMSProject
 
         private void TextBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
+            var comboBoxSearchName = ComboBoxSearch.SelectedItem.ToString().Split(':')[1].TrimStart(' ');
+            if (comboBoxSearchName.Equals("Medicine id"))
+            {
+                var resultList = Medicines.Where(medicine => medicine.MedicineId.ToLower().Contains(TextBoxSearch.Text.ToLower()));
+                dataGridMedicines.ItemsSource = resultList;
+                return;
+            }
+            if (comboBoxSearchName.Equals("Medicine name"))
+            {
+                var resultList = Medicines.Where(medicine => medicine.MedicineName.ToLower().Contains(TextBoxSearch.Text.ToLower()));
+                dataGridMedicines.ItemsSource = resultList; return;
+            }
+            if (comboBoxSearchName.Equals("Manufacturer"))
+            {
+                var resultList = Medicines.Where(medicine => medicine.Manufacturer.ToLower().Contains(TextBoxSearch.Text.ToLower()));
+                dataGridMedicines.ItemsSource = resultList; return;
+            }
+            if (comboBoxSearchName.Equals("Price range"))
+            {
+                //kod za pretragu
+                return;
 
+            }
+            if (comboBoxSearchName.Equals("Quantity"))
+            {
+                var resultList = Medicines.Where(medicine => medicine.Quantity.ToString().Contains(TextBoxSearch.Text));
+                dataGridMedicines.ItemsSource = resultList; return;
+            }
+            if (comboBoxSearchName.Equals("Ingredients"))
+            {
+                //kod za pretragu
+                return;
+            }
         }
 
         private void OpenMedicalRecord(object sender, RoutedEventArgs e)
@@ -97,23 +129,24 @@ namespace SiMSProject
 
         private void SortBy(object sender, SelectionChangedEventArgs e)
         {
+            var comboBoxSortName = ComboBoxSort.SelectedItem.ToString().Split(':')[1].TrimStart(' ');
             Console.WriteLine(ComboBoxSort.SelectedItem);
 
-            if (ComboBoxSort.SelectedItem.ToString().Contains("Medicine name"))
+            if (comboBoxSortName.Equals("Medicine name"))
             {
                 MedicineList.Sort((x, y) => x.MedicineName.CompareTo(y.MedicineName));
                 SortListToObsCollection();
                 return;
             }
 
-            if (ComboBoxSort.SelectedItem.ToString().Contains("Price"))
+            if (comboBoxSortName.Equals("Price"))
             {
                 MedicineList.Sort((x, y) => x.Price.CompareTo(y.Price));
                 SortListToObsCollection();
                 return;
             }
             
-            if (ComboBoxSort.SelectedItem.ToString().Contains("Quantity in stock"))
+            if (comboBoxSortName.Equals("Quantity in stock"))
             {
                 MedicineList.Sort((x, y) => x.QuantityInStock.CompareTo(y.QuantityInStock));
                 SortListToObsCollection();
